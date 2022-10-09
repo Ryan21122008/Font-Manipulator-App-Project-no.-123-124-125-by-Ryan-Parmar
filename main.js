960b32e72ba6b797e1c8e338e15df991a5c6bcfe
Ryan21122008 <77188055+Ryan21122008@users.noreply.github.com>
2022-10-09 13:49:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nosex = 0;
+nosey = 0;
+leftwristx = 0;
+rightwristx = 0;
+difference = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(1000, 1000);
@@ -12,5 +17,18 @@ function modelloaded(){
 function gotposes(result){
     if(result.length > 0){
         console.log(result);
+        nosex = result[0].pose.nose.x;
+        nosey = result[0].pose.nose.y;
+        leftwristx = result[0].pose.leftWrist.x;
+        rightwristx = result[0].pose.rightWrist.x;
+        difference = floor(leftwristx - rightwristx);
+        document.getElementById("textsize").innerHTML = "The size of the text is " + difference + "px";
     }
+}
+function draw(){
+    background("rgb(245, 245, 220)");
+    fill("#ffc421");
+    stroke("#fcff33");
+    textSize(difference);
+    text("Ryan Parmar", nosex, nosey);
 }
